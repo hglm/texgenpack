cf548ef583ca9592a55ea217b0ec43a2e25b9cbe
Harm Hanemaaijer <fgenfb@yahoo.com>
2015-06-03 15:04:27 +0200

    Bump version to v0.9.6
    Signed-off-by: Harm Hanemaaijer <fgenfb@yahoo.com>

--- texgenpack.c
+++ texgenpack.c
@@ -71,7 +71,7 @@ int option_perceptive = 1;
 int option_deterministic = 0;
 
 static char *instructions1 =
-"texgenpack v0.9.5 -- Texture conversion and compression using a genetic algorithm.\n"
+"texgenpack v0.9.6 -- Texture conversion and compression using a genetic algorithm.\n"
 "Usage: texgenpack <command> <options> <source filename> <destination filename>.\n"
 "\n"
 "Commands:\n";
